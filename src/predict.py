881f3fb9eeace62bb7beaec6f1cb8fa654af8d67
ValdemarSergeevich <valdemarsergeevich@gmail.com>
2026-05-04 16:22:46 +0200

    AAAAAAAAAAAAAAAAAAAAAAA

--- src/predict.py
+++ src/predict.py
@@ -132,4 +132,4 @@ def predict(form: Form):
 # ---------------------------------------------------------
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 8080))
-    uvicorn.run("src.predict:app", host="0.0.0.0", port=port)
+    uvicorn.run(app, host="0.0.0.0", port=port)
